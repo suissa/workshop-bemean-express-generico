770eebc8f07a333b5ec4d90362fa6d5227ca4920
Suissa <jnascimento@gmail.com>
2013-09-29 14:09:02 -0300

    Model refatorado

--- models/beer.js
+++ models/beer.js
@@ -18,4 +18,78 @@ var BeerSchema = new Schema({
   type: { type: String, default: '' }
 });
 
-exports.model = mongoose.model('Beer', BeerSchema);
+var Model = mongoose.model('Beer', BeerSchema);
+
+exports.find = function(req, res){
+
+  var query = {};
+
+  Model.find(query, function (err, beers) {
+    if(err) {
+      console.log('Houve algum erro, tente novamente', err);
+    } else {
+      res.json(beers);
+    }
+  });
+}; 
+
+exports.retrieve = function(req, res){
+
+  var id = req.params.id;
+  var query = {_id: id}
+
+  Model.findOne(query, function (err, beer) {
+    console.log('achou algo?');
+    if(err) {
+      console.log('Houve algum erro, tente novamente', err);
+    } else {
+      res.json(beer);
+    }
+  });
+
+}
+
+exports.create = function(req, res){
+  var dados = req.body;
+  var beer = new Model(dados);
+
+  beer.save(function(err, data) {
+    if(err){
+      console.log(err);
+      res.render("beer_list", {msg: err})
+    } else {
+      res.json(data);
+    }
+  });
+};
+
+exports.update = function(req, res){
+
+  var id = req.params.id;
+  var dados = req.body;
+  var query = {_id: id};
+
+  Model.update(query, dados, function(err, beer) {
+    if(err) {
+      console.log(err);
+    } else {
+      // res.render('beer_update', {cerveja: beer});
+      res.json(beer);
+    }
+  });
+
+}
+
+exports.delete = function(req, res){
+
+  var id = req.params.id;
+  var query = {_id: id};
+
+  Model.remove({_id: id}, function(err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+}
--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,5 @@
-var Beer = require("../models/beer.js").model;
+var _model = require("../models/beer.js");
 
-var _model = {};
-_model.find = function(req, res){
-
-  var query = {};
-
-  Beer.find(query, function (err, beers) {
-    if(err) {
-      console.log('Houve algum erro, tente novamente', err);
-    } else {
-      res.json(beers);
-    }
-  });
-}; 
-
-_model.retrieve = function(req, res){
-
-  var id = req.params.id;
-  var query = { "_id": id};
-
-  Beer.findOne(query, function (err, beer) {
-    console.log('achou algo?');
-    if(err) {
-      console.log('Houve algum erro, tente novamente', err);
-    } else {
-      res.json(beer);
-    }
-  });
-  
-}
-
-_model.create = function(req, res){
-  var dados = req.body;
-  var beer = new Beer(dados);
-
-  beer.save(function(err, data) {
-    if(err){
-      console.log(err);
-      res.render("beer_list", {msg: err})
-    } else {
-      res.json(data);
-    }
-  });
-};
-
-_model.delete = function(req, res, id){
-  Beer.remove({_id: id}, function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      var mensagem = 'Cerveja deletada com sucesso';
-      _model.find(req, res, mensagem);
-    }
-  });
-}
 
 exports.create = function(req, res){
   _model.create(req, res);
@@ -68,28 +14,11 @@ exports.list = function(req, res){
 }
 
 exports.update = function(req, res){
-// UPDATE
-  // ObjectId("522fc571ffd8647932000001")
-  // var beer_id = "522fc5f0542a6bf832000001";
-  var beer_id = req.params.id;
-  var dados = req.body;
-
-  console.log("dados", dados);
-  Beer.update({_id: beer_id}, dados, function(err, beer) {
-    if(err) {
-      console.log(err);
-    } else {
-      // res.render('beer_update', {cerveja: beer});
-      _model.find(req, res); // refatorado
-
-    }
-  });
+  _model.update(req, res);
 }
 
-exports.delete = function(req, res, id){
-  // DELETE
-  var beer_id = req.params.id;
-  _model.delete(req, res, beer_id);
+exports.delete = function(req, res){
+  _model.delete(req, res);
 }
 
 
